490adf4f5421c0304da00ce842347357cfc93d21
Logan Bonney <wakeup2early@gmail.com>
2015-10-15 14:00:36 -0600

    made the HQ research defusion at beginning of the game

--- HQ.java
+++ HQ.java
@@ -3,30 +3,38 @@ package team018;
 import battlecode.common.Direction;
 import battlecode.common.MapLocation;
 import battlecode.common.RobotController;
+import battlecode.common.Upgrade;
 
-public class HQ extends DefaultRobot{
-	public HQ(RobotController rc){
+public class HQ extends DefaultRobot {
+	public int roundCount = 0;
+
+	public HQ(RobotController rc) {
 		super(rc);
 	}
-	
-	public void run(){
-		while(true){
+
+	public void run() {
+		while (true) {
 			MapLocation location = rc.getLocation();
-			try{
-				// spawn soldier
-				Direction dir = location.directionTo(enemyHQLoc);
-				
-				dir = getDirTowTarAvoidMines(dir);
-				if(dir == null){
-					dir = randDirNoMines();
+			try {
+				if (roundCount < 45 && roundCount != 0 && roundCount != 30) {
+					rc.researchUpgrade(Upgrade.DEFUSION);
+				} else {
+					// spawn soldier
+					Direction dir = location.directionTo(enemyHQLoc);
+
+					dir = getDirTowTarAvoidMines(dir);
+					if (dir == null) {
+						dir = randDirNoMines();
+					}
+					if (dir == null) {
+						dir = directions[rand.nextInt(8)];
+					}
+					rc.spawn(dir);
 				}
-				if(dir == null){
-					dir = directions[rand.nextInt(8)];
-				}
-				rc.spawn(dir);
-			} catch(Exception e){
+			} catch (Exception e) {
 				e.printStackTrace();
 			}
+			roundCount++;
 			rc.yield();
 		}
 	}
