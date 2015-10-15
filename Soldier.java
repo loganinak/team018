051dc293d68e5366399aa3e9ebeb6f975b7e12d7
Logan Bonney <wakeup2early@gmail.com>
2015-10-14 21:04:22 -0600

    Fixed the current game losing errors
    Made the HQ only spawn in directions that contain no mines. Also made
    the Robots go in random directions if things are in the way.

--- Soldier.java
+++ Soldier.java
@@ -1,12 +1,15 @@
 package team018;
 
 import battlecode.common.Direction;
+import battlecode.common.GameActionException;
 import battlecode.common.MapLocation;
 import battlecode.common.RobotController;
 
 public class Soldier extends DefaultRobot {
+	private Direction dirToEnemHQ;
 	public Soldier(RobotController rc) {
 		super(rc);
+		dirToEnemHQ = rc.getLocation().directionTo(enemyHQLoc);
 	}
 
 	public void run() {
@@ -14,24 +17,29 @@ public class Soldier extends DefaultRobot {
 			MapLocation location = rc.getLocation();
 			try {
 				// sense and destroy mines
-				if(rc.canMove(location.directionTo(enemyHQLoc)) == false && location.distanceSquaredTo(enemyHQLoc) > 5){
-					if(rc.senseMine(location.add(Direction.NORTH)) == null){
-						rc.move(Direction.NORTH);
-					} else {
-						rc.defuseMine(location.add(Direction.NORTH));
-					}
-				
-
-				} else if (rc.senseMine(location.add(location.directionTo(rc.senseEnemyHQLocation()))) != null) {
-					rc.defuseMine(location.add(location.directionTo(rc.senseEnemyHQLocation())));
-				} else {
-					rc.move(location.directionTo(rc.senseEnemyHQLocation()));
-					rc.setIndicatorString(0, "test");
+				if(rc.canMove(dirToEnemHQ) == false && location.distanceSquaredTo(enemyHQLoc) > 5){
+					moveRandomly();
+				} else if(rc.senseMine(location.add(dirToEnemHQ)) != null){
+					rc.defuseMine(location.add(dirToEnemHQ));
+				} else if (location.distanceSquaredTo(enemyHQLoc) > 5){
+					rc.move(dirToEnemHQ);
+				} else{
+					rc.move(rc.getLocation().directionTo(enemyHQLoc));
 				}
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
 			rc.yield();
+		}
+	}
+	
+	private void moveRandomly() throws GameActionException{
+		int randInt = rand.nextInt(8);
+		MapLocation spot = rc.getLocation().add(directions[randInt]);
+		if(rc.senseMine(spot) == null){
+			rc.move(directions[randInt]);
+		} else {
+			rc.defuseMine(spot);
 		}
 	}
 }
