f9d441e1885ba6b3628610b5157f9d17f882666a
Logan Bonney <wakeup2early@gmail.com>
2015-10-15 02:01:20 -0600

    Added support for defenders and builders under the soldier class
    Soldiers and builders currently don't do anything. I also changed HQloc
    to HQLoc.

--- Soldier.java
+++ Soldier.java
@@ -1,49 +1,92 @@
 package team018;
 
 import battlecode.common.Direction;
+import battlecode.common.GameActionException;
 import battlecode.common.MapLocation;
 import battlecode.common.RobotController;
 
 public class Soldier extends DefaultRobot {
-	
+
 	private Direction dirToEnemHQ;
 	private Direction movingDir;
 	private Direction lastMovingDir;
-	
+
+	private final int soldier = 0;
+	private final int defender = 1;
+	private final int builder = 2;
+	private int task;
+
 	public Soldier(RobotController rc) {
 		super(rc);
+		int temp = rand.nextInt(5);
+		if (temp == 0) {
+			temp = rand.nextInt(2);
+			if (temp == 0) {
+				task = defender;
+			} else {
+				task = builder;
+			}
+		} else {
+			task = soldier;
+		}
+		String role;
+		switch (task) {
+			case 0:
+				role = "Soldier";
+				break;
+			case 1:
+				role = "Defender";
+				break;
+			case 2:
+				role = "builder";
+				break;
+			default:
+				role = "None";
+				break;
+		}
+
+		rc.setIndicatorString(0, "Role: " + role);
 	}
 
 	public void run() {
 		while (true) {
-			dirToEnemHQ = rc.getLocation().directionTo(enemyHQLoc);
 			MapLocation location = rc.getLocation();
 			try {
-				lastMovingDir = movingDir;
-				movingDir = getDirTowTarAvoidMines(dirToEnemHQ);
-				MapLocation movingLoc = location.add(movingDir);
-				
-				boolean mine = rc.senseMine(movingLoc) != null;
-				if(mine){
-					rc.defuseMine(movingLoc);
+				if (task == soldier) {
+					dirToEnemHQ = rc.getLocation().directionTo(enemyHQLoc);
 					lastMovingDir = movingDir;
-				} else if(rc.canMove(movingDir) && !mine){
-					rc.move(movingDir);
-					lastMovingDir = movingDir;
-				} else {//does this if it can't move towards the base
-					movingLoc = location.add(lastMovingDir);
-					mine = rc.senseMine(movingLoc) != null;
-					if(mine){
+					movingDir = getDirTowTarAvoidMines(dirToEnemHQ);
+					MapLocation movingLoc = location.add(movingDir);
+
+					boolean mine = rc.senseMine(movingLoc) != null;
+					if (mine) {
 						rc.defuseMine(movingLoc);
-					} else if(rc.canMove(movingDir) && !mine){
-						rc.move(lastMovingDir);
-					}						
+						lastMovingDir = movingDir;
+					} else if (rc.canMove(movingDir) && !mine) {
+						rc.move(movingDir);
+						lastMovingDir = movingDir;
+					} else {// does this if it can't move towards the base
+						movingLoc = location.add(lastMovingDir);
+						mine = rc.senseMine(movingLoc) != null;
+						if (mine) {
+							rc.defuseMine(movingLoc);
+						} else if (rc.canMove(movingDir) && !mine) {
+							rc.move(lastMovingDir);
+						}
+					}
+				} else if (task == defender) {
+					patrol(HQLoc, 2);
+				} else if (task == builder) {
+					rc.move(Direction.NORTH);
 				}
-				
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
 			rc.yield();
 		}
+	}
+
+	private void patrol(MapLocation target, int Distance) throws GameActionException {
+		rc.move(directions[rand.nextInt(8)]);
 	}
 }
